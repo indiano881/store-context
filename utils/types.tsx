73ab6a194c9832b77c04ec6174850ae8beaf70db
davidebaldi <d.baldi88@gmail.com>
2024-09-25 20:58:34 +0200

    types and registered users done

---
+++ utils/types.tsx
@@ -0,0 +1,6 @@
+export type UserType = {
+    name:string,
+    password:string,
+    category: string,
+    saveditems: string[]
+}
---
+++ utils/users.tsx
@@ -0,0 +1,17 @@
+import { UserType } from "./types";
+
+export const registeredUsers:UserType[] =[
+    {
+        name: "Davide",
+        password: "123456",
+        category: "groceries",
+        saveditems:[]
+    },
+    {
+        name: "Rob",
+        password: "123456",
+        category: "furniture",
+        saveditems:[]
+    },
+
+] 
