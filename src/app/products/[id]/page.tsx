e8307b1b849bc6e581e1891a4cae762ddc5cc6e2
davidebaldi <d.baldi88@gmail.com>
2024-09-26 12:02:35 +0200

    id visible in single product

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,5 +1,9 @@
-const SingleProduct = () => {
-    return <h2>SINGLW PRODUCT</h2>
+const SingleProduct = ({params}:{params:{id:string}}) => {
+    const {id} =params;
+    return <>
+    <h2>SINGLW PRODUCT</h2>
+    <h4>{id}</h4>
+    </>
 }
 
 export default SingleProduct;
