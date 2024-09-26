44f4ba1bff03ba9481d402d21865f04ecb24e70c
davidebaldi <d.baldi88@gmail.com>
2024-09-26 08:02:14 +0200

    save progress video 1h 16 min

--- utils/contexts.tsx
+++ utils/contexts.tsx
@@ -4,9 +4,10 @@ import { createContext, useContext, useState} from "react";
 import { UserContextType, UserType } from "./types";
 
 
-//context runs as first, very deep
+//step1-context runs as first, very deep-INITIALIZATION
 const UserContext = createContext<UserContextType | null>(null)
 
+//step2-here gives value
 export const UserProvider = ({children}:{children:React.ReactNode}) => {
     
     const [user,setUser] = useState<UserType | null>(null)
@@ -17,4 +18,9 @@ export const UserProvider = ({children}:{children:React.ReactNode}) => {
         </UserContext.Provider>
 
     )
+}
+
+//step3-this is the function other componetns can call
+export const useUserContext = () => {
+    return useContext(UserContext)
 }
