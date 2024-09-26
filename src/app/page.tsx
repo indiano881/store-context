163dfcfbf1a0de932000355ddd83e864a39bf2bf
davidebaldi <d.baldi88@gmail.com>
2024-09-26 11:42:12 +0200

    saved all the fetches

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,11 @@ import { useEffect } from "react";
 export default function Home() {
 
   useEffect(()=>{
-    fetchData("https://dummyjson.com/products")
+    fetchData("https://dummyjson.com/products?limit=0")//working get all 194 products use limit=25 or x2 x4
+    fetchData('https://dummyjson.com/products/category/smartphones')//working
+    fetchData('https://dummyjson.com/products/categories')
   },[])
-
+//data.meals.slice(0,5) for example or sort by review
   return (<>
   <h1>connected</h1>
   
