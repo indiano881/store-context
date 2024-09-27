944d24bff585821d356121e8ab10c9337597bcb3
davidebaldi <d.baldi88@gmail.com>
2024-09-27 14:10:24 +0200

    footer done

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -61,7 +61,7 @@ const Footer = () => {
             </div>
 
             <div>
-                <p className="text-sm font-semibold tracking-widest text-gray-400 uppercase">Company</p>
+                <p className="text-sm font-bold tracking-widest uppercase">Company</p>
 
                 <ul className="mt-6 space-y-4">
                     <li>
@@ -83,29 +83,29 @@ const Footer = () => {
             </div>
 
             <div>
-                <p className="text-sm font-semibold tracking-widest text-gray-400 uppercase">Help</p>
+                <p className="text-sm font-bold tracking-widest  uppercase">Help</p>
 
                 <ul className="mt-6 space-y-4">
                     <li>
-                        <a href="#" title="" className="flex text-base text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"> Customer Support </a>
+                        <a href="#" title="" className="flex text-base text-black transition-all duration-200 hover:text-blue-600 "> Customer Support </a>
                     </li>
 
                     <li>
-                        <a href="#" title="" className="flex text-base text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"> Delivery Details </a>
+                        <a href="#" title="" className="flex text-base text-black transition-all duration-200 hover:text-blue-600 "> Delivery Details </a>
                     </li>
 
                     <li>
-                        <a href="#" title="" className="flex text-base text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"> Terms & Conditions </a>
+                        <a href="#" title="" className="flex text-base text-black transition-all duration-200 hover:text-blue-600 "> Terms & Conditions </a>
                     </li>
 
                     <li>
-                        <a href="#" title="" className="flex text-base text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"> Privacy Policy </a>
+                        <a href="#" title="" className="flex text-base text-black transition-all duration-200 hover:text-blue-600 "> Privacy Policy </a>
                     </li>
                 </ul>
             </div>
 
             <div className="col-span-2 md:col-span-1 lg:col-span-2 lg:pl-8">
-                <p className="text-sm font-semibold tracking-widest text-gray-400 uppercase">Subscribe to newsletter</p>
+                <p className="text-sm font-bold tracking-widest text-pt-primary uppercase">Subscribe to newsletter</p>
 
                 <form action="#" method="POST" className="mt-6">
                     <div>
@@ -113,14 +113,14 @@ const Footer = () => {
                         <input type="email" name="email" id="email" placeholder="Enter your email" className="block w-full p-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600" />
                     </div>
 
-                    <button type="submit" className="inline-flex items-center justify-center px-6 py-4 mt-3 font-semibold text-white transition-all duration-200 bg-blue-600 rounded-md hover:bg-blue-700 focus:bg-blue-700">Subscribe</button>
+                    <button type="submit" className="inline-flex items-center justify-center px-6 py-4 mt-3 font-semibold text-white transition-all duration-200 bg-pt-primary rounded-md hover:bg-blue-700 focus:bg-blue-700">Subscribe</button>
                 </form>
             </div>
         </div>
 
         <hr className="mt-16 mb-10 border-gray-200" />
 
-        <p className="text-sm text-center text-gray-600">© Copyright 2021, All Rights Reserved by Postcraft</p>
+        <p className="text-sm text-center italic">© Copyright 2024, All Rights Reserved by Pandorum AB</p>
     </div>
 </section>)
 }
